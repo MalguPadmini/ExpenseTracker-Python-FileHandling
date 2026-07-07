850401876f8ce1b163fd97ab58e76c5b49aa9642
MalguPadmini <24f3003164@ds.study.iitm.ac.in>
2026-07-07 21:35:13 +0530

    Build command-line Expense Tracker using python #V.01

---
+++ main.py
@@ -0,0 +1,66 @@
+
+def add_expense():
+    amount = float(input("Enter the amount spent: "))
+    category = input("Enter the category of expense (e.g., Food, Transport, Entertainment): ")
+
+    with open("expenses.csv", "a") as file:
+        file.write(f"{amount},{category}\n")
+
+    print("\nExpense Added Successfully!\n")
+    print(f"Amount: ₹{amount:.2f}")
+    print(f"Category: {category}")
+
+def view_expenses():
+    print("\nAll Expenses:\n")
+    with open("expenses.csv", "r") as file:
+        for line in file:
+            amount, category = line.strip().split(",")
+            print(f"Amount: ₹{amount}, Category: {category}")
+
+def view_total():
+    total = 0
+    with open("expenses.csv", "r") as file:
+        for line in file:
+            amount, category = line.strip().split(",")
+            total += float(amount)
+
+    print(f"\nTotal Expenses: ₹{total:.2f}")
+
+def delete_all_expenses():
+    choice1 = input("Enter 'y'-> yes OR 'n'->no and go back to menu:")
+    if choice1.lower() == "y":
+        with open("expenses.csv", "w") as file:
+            file.write("")
+        print("\nAll expenses deleted successfully!")
+    elif choice1.lower() == "n":
+        print("\nDeletion Cancelled")
+    else:
+        print("\nInvalid input")
+
+while True:
+
+    print("=" * 40)
+    print("      Expense Tracker")
+    print("=" * 40)
+
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. View Total")
+    print("4. Delete all expenses")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+    if choice=="5":
+        print("Thank you for using Expense Tracker!")
+        break
+    elif choice=="1":
+        add_expense()
+    elif choice=="2":
+        view_expenses()
+    elif choice=="3":
+        view_total()
+    elif choice=="4":
+        delete_all_expenses()
+    else:
+        print("Invalid choice. Please try again.")
+
